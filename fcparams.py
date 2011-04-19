716d1719216c72820e6a1ae5a13868535e9a6d8f
Robert Flanagan <rrf4@buffalo.edu>
2011-04-19 11:42:21 -0500

    Added the fuel cycle code and parameters

---
+++ fcparams.py
@@ -0,0 +1,52 @@
+from bright import lwr_defaults
+LWR_Params = lwr_defaults()
+
+from bright import fr_defaults
+FR_Params = fr_defaults()
+
+#General Specifications
+Quiet = True
+
+#LWR Specifications
+LWR_Params.BUt =              51.0 
+LWR_Params.batches   =        3 
+LWR_Params.pnl      =         0.98 
+
+LWR_Fuel2Mod  =  0.30146786330
+                            
+#LWR Storage                
+LWR_SNF_Storage_Time        =  6
+                            
+#LWR Reprocessing                         
+LWR_SE_U      =              0.999 
+LWR_SE_NP     =              0.999 
+LWR_SE_PU     =              0.999 
+LWR_SE_AM     =              0.999 
+LWR_SE_CM     =              0.999 
+LWR_SE_CS     =              0.9999 
+LWR_SE_SR     =              0.9999 
+                            
+#FR Specifications                        
+#FR_Params.BUt = 176.0						#FR Burnup
+FR_Params.BUt = 180.0						#FR Burnup
+FR_Params.batches = 3						#Number of FR batches 
+FR_Params.pnl =  0.65		 				#FR Non-Leakage Probability
+
+FR_TRU_CR     =  0.500000
+FR_LAN_FF_Cap =  0.0 * (10**-6)
+                            
+#FR Storage                 
+FR_SNF_Storage_Time  = 3
+                            
+#FR Reprocessing                          
+FR_SE_U       =              0.999 
+FR_SE_NP      =              0.999 
+FR_SE_PU      =              0.999 
+FR_SE_AM      =              0.9 
+FR_SE_CM      =              0.999 
+FR_SE_CS      =              0.9999 
+FR_SE_SR      =              0.9999 
+
+
+#Interim Storage
+INT_SNF_Storage_Time        =  30
