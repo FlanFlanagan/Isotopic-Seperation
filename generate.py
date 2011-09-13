5bad22c8026bd8c5f8bf42b2549129b4539dd27e
Robert Flanagan <rrf4@buffalo.edu>
2011-09-13 16:56:19 -0500

    Removed some of the reduntant code

--- generate.py
+++ generate.py
@@ -4,17 +4,18 @@ import mass_stream as MS
 h=os.getcwd()
 a = MS.MassStream('CooledIsos.txt', 1)
 for keys in a.comp.keys():    
-   if keys/10000 > 88:
-      try:
-	os.mkdir(str(keys))
-	os.chdir(str(keys))
-	print(str(keys))
-	writer = open('rmIsos.py','w')
-	writer.write('RmIsos =' + str(keys))
-	writer.close
-	writer2 = open('BUd.py','w')
-	writer2.close
-      except OSError:
-	pass
-   
+   if a.comp[keys] >1E-20:
+      if keys/10000 > 88:
+	  try:
+	    os.mkdir(str(keys))
+	    os.chdir(str(keys))
+	    print(str(keys))
+	    writer = open('rmIsos.py','w')
+	    writer.write('RmIsos =' + str(keys))
+	    writer.close
+	    writer2 = open('BUd.py','w')
+	    writer2.close
+	  except OSError:
+	    pass
+      
    os.chdir(h)
